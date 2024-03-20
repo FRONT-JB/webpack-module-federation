44dce5b5635b90b65168456b10ad2cdb8a518d1e
전병길 <jeonbg@kakao.com>
2024-03-21 01:21:32 +0900

    feat(apps): ErrorBoundary 추가

--- apps/main-app/src/App.jsx
+++ apps/main-app/src/App.jsx
@@ -5,6 +5,7 @@ import { join, map } from "lodash";
 import "./index.css";
 
 import { NameProvider } from "shared-library";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Button = React.lazy(() => import("component_app/Button"));
 
@@ -16,18 +17,24 @@ const App = () => (
       <div>Language: JavaScript</div>
       <div>CSS: Empty CSS</div>
       <div>{join(map(["1", "2"]), "-")}</div>
-      <Suspense fallback="...loading">
-        <Button
-          onClick={() => {
-            console.log("Clicked!!");
-          }}
-        >
-          Primary
-        </Button>
-      </Suspense>
-      <Suspense fallback="...loading">
-        <Button type="warning">Warning</Button>
-      </Suspense>
+
+      <ErrorBoundary fallback={<div>Error 1</div>}>
+        <Suspense fallback="...loading">
+          <Button
+            onClick={() => {
+              console.log("Clicked!!");
+            }}
+          >
+            Primary
+          </Button>
+        </Suspense>
+      </ErrorBoundary>
+
+      <ErrorBoundary fallback={<div>Error 2</div>}>
+        <Suspense fallback="...loading">
+          <Button type="warning">Warning</Button>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   </NameProvider>
 );
